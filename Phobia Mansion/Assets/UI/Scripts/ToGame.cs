73da3d59ca048191ec69391730cbd6305bf104eb
MacKenzie1976 <97097945@st.deltion.nl>
2023-05-22 16:22:36 +0200

    UI & START BUTTON
    Kan nu naar main game

---
+++ Phobia Mansion/Assets/UI/Scripts/ToGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGame : MonoBehaviour
+{
+    
+    public void ToMainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
