6e54f63065c119e3be7feb6344a603a1a29c9e68
R4HK00N <robert.eddy.beltman@gmail.com>
2023-05-30 14:30:09 +0200

    aan eigen scene gewerkt
    ik heb deur animaties in elkaar gezet

--- Phobia Mansion/Assets/Scripts/interacting/DoorInteraction.cs
+++ Phobia Mansion/Assets/Scripts/interacting/DoorInteraction.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class DoorInteraction : MonoBehaviour
 {
     public Interactable interaction;
-
+    public Animator padlockAnimator;
+    public Animator doorOpening;
     public void Update()
     {
         if (interaction.interactable == true)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Destroy(gameObject);
+                padlockAnimator.SetTrigger("PadLockFalling");
+                doorOpening.SetTrigger("DoorOpen");
             }
         }
     }
