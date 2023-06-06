79f4e4a6d445b771256c636721580eb9d6284a65
MacKenzie1976 <97097945@st.deltion.nl>
2023-06-06 15:34:41 +0200

    UI
    Interact pop up

--- Phobia Mansion/Assets/Scripts/interacting/main interacting/Interaction.cs
+++ Phobia Mansion/Assets/Scripts/interacting/main interacting/Interaction.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 public class Interaction : MonoBehaviour
 {
     public GameObject crosshair1, crosshair2;
+    public TMP_Text interact;
     RaycastHit hit;
     public bool canInteract;
     public LayerMask layer;
@@ -17,13 +19,17 @@ public class Interaction : MonoBehaviour
         {
             crosshair1.SetActive(false);
             crosshair2.SetActive(true);
+            interact.enabled = true;
             canInteract = true;
+            
         }
         else
         {
             crosshair1.SetActive(true);
             crosshair2.SetActive(false);
+            interact.enabled = false;
             canInteract = false;
+            
         }
     }
 }
