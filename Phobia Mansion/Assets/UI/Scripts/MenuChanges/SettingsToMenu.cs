81f26a211196f0b41d8e9b04d8c934a1c1aecb0f
MacKenzie1976 <97097945@st.deltion.nl>
2023-06-14 17:00:11 +0200

    UX & UI
    Audio aaaaaauuuuugh

--- Phobia Mansion/Assets/UI/Scripts/MenuChanges/SettingsToMenu.cs
+++ Phobia Mansion/Assets/UI/Scripts/MenuChanges/SettingsToMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuBack2 : MonoBehaviour
+public class SettingsToMenu : MonoBehaviour
 {
     public GameObject screen1;
     public GameObject screen2;
