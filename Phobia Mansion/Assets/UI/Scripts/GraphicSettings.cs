93b5d8ff8f002826bc7bb5e2ca5ecd3988557423
R4HK00N <robert.eddy.beltman@gmail.com>
2023-06-28 16:41:36 +0200

    resolution fixed

--- Phobia Mansion/Assets/UI/Scripts/GraphicSettings.cs
+++ Phobia Mansion/Assets/UI/Scripts/GraphicSettings.cs
@@ -6,34 +6,52 @@ using TMPro;
 
 public class GraphicSettings : MonoBehaviour
 {
-
-
-    public TMP_Dropdown resolutionDropdown;
-
-    public Resolution[] resolutions;
-
-    
+    [Header("Resolutions")]
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    private List<Resolution> filteredResolution;
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
 
     public void Start()
     {
+
         resolutions = Screen.resolutions;
+        filteredResolution = new List<Resolution>();
 
         resolutionDropdown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if (resolutions[i].refreshRate == currentRefreshRate)
+            {
+                filteredResolution.Add(resolutions[i]);
+            }
+        }
 
         List<string> options = new List<string>();
-
-        for ( int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < filteredResolution.Count; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            string resolutionOption = filteredResolution[i].width + "X" + filteredResolution[i].height + " " + filteredResolution[i].refreshRate + "Hz";
+            options.Add(resolutionOption);
+
+            if (filteredResolution[i].width == Screen.width && filteredResolution[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
-
-
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
-
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolution[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetFullScreen (bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
